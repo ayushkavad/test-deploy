6448d1cbc2ab7f7a3d9d48a83d71d5d2b121b70d
byteayush <ayush@advant.xyz>
2025-06-23 22:47:01 +0530

    change env

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   /* config options here */
+  env: {
+    NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
+  },
 };
 
 export default nextConfig;
