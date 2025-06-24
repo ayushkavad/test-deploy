8398eecb36a12f9d7f2a4b74b29aced09f3fd95e
byteayush <ayush@advant.xyz>
2025-06-24 22:09:50 +0530

    added new version

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,16 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-      </main>
-      <div>
-        {process.env.NEXT_PUBLIC_APP_URL === 'development' && (
-          <div>
-            <h1>We are in Development</h1>
-          </div>
-        )}
-        {process.env.NEXT_PUBLIC_APP_URL === 'production' && (
-          <div>
-            <h1>We are in Production</h1>
-          </div>
-        )}
-      </div>
+      {process.env.NEXT_PUBLIC_APP_URL === 'development' && (
+        <div>
+          <h1>We are in Development</h1>
+        </div>
+      )}
+      {process.env.NEXT_PUBLIC_APP_URL === 'production' && (
+        <div>
+          <h1>We are in Production</h1>
+        </div>
+      )}
     </div>
   );
 }
