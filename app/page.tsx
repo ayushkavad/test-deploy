0e439c1d32b10ffc87aaf7e6c62f4be20e56e4a2
byteayush <ayush@advant.xyz>
2025-06-24 15:41:16 +0530

    change tf file

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,23 @@ export default function Home() {
           </div>
         )}
       </div>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+      </footer>
     </div>
   );
 }
